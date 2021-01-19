dc2532bb5fcffbcf9f343d2c146fab925d3b9adb
Rawle Springer <rspringerdeveloper@gmail.com>
2021-01-19 02:24:34 -0500

    Added logic for dynamic grid and clear grid button

--- index.js
+++ index.js
@@ -1,25 +1,58 @@
 //grid
-
+let gridUnit = 16;
+let numberOfSquares = gridUnit * gridUnit;
+//
 const containerDOM = document.querySelector(".container");
 
-const container = [];
+const button = document.querySelector("button");
+// const container = [];
 
 const gridSquare = document.createElement("div");
 gridSquare.classList.add("square");
 
-for (let index = 0; index < 256; index++) {
+//initial
+for (let index = 0; index < numberOfSquares; index++) {
   const clone = gridSquare.cloneNode(gridSquare);
   clone.setAttribute("data-id", index.toString());
+  clone.style.height = `${960 / gridUnit - 2}px`;
+  clone.style.width = `${960 / gridUnit - 2}px`;
   containerDOM.appendChild(clone);
-  container.push(clone);
+  // container.push(clone);
   clone.addEventListener("mousemove", (e) => {
     clone.classList.add("active");
   });
 }
 
-const array1 = Array.from(container);
-console.log(array1[3]);
-/* 
-when user moves mouse to a square change the class of the 
-square to a different color
-*/
+button.addEventListener("click", () => {
+  const squares = document.querySelectorAll(".square");
+  squares.forEach((square) => {
+    square.classList.remove("active");
+  });
+
+  let userInput = prompt("How many squares for your grid?");
+  gridUnit = parseInt(userInput);
+  if (gridUnit > 100) {
+    gridUnit = parseInt(prompt("Number must be no more than 100"));
+    if (gridUnit > 100) {
+      return;
+    }
+  }
+  numberOfSquares = parseInt(userInput) * parseInt(userInput);
+  while (containerDOM.firstChild) {
+    containerDOM.removeChild(containerDOM.firstChild);
+  }
+
+  for (let index = 0; index < numberOfSquares; index++) {
+    let unit = 960 / gridUnit - 2;
+
+    const clone = gridSquare.cloneNode(gridSquare);
+    clone.setAttribute("data-id", index.toString());
+    clone.style.height = `${unit}px`;
+    clone.style.width = `${unit}px`;
+    containerDOM.appendChild(clone);
+    // container.push(clone);
+    clone.addEventListener("mousemove", (e) => {
+      clone.classList.add("active");
+    });
+  }
+});
